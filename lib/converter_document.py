a336f054444c65f43a13dd205f103fbcb792a93b
galihmrd <galihmardiana15@gmail.com>
2022-01-23 15:19:45 +0700

    improvement - v1.0.4

--- lib/converter_document.py
+++ lib/converter_document.py
@@ -2,7 +2,6 @@ from pyrogram.types import Message
 from pyrogram import Client, filters
 
 from aspose import words
-from pyromod import listen
 
 
 @Client.on_message(filters.command("pdf2docx"))
@@ -26,14 +25,17 @@ async def pdf2docx_(client, message):
 
 @Client.on_message(filters.command("text2docx"))
 async def text2docx_(client, message):
-    input = await client.ask(msg.chat.id,'Silahkan Tulis dan kirim teks yang akan dikonversi!', filters=filters.text, parse_mode='Markdown', disable_web_page_preview=True)
-    msg = await message.reply("Memproses...")
+    try:
+        input = " ".join(message.command[1:])
+        msg = await message.reply("Memproses...")
+    except BaseException:
+        await msg.edit("Input tidak kosong!")
     # Blank document
     doc = words.Document()
     # Write a document
     try:
         builder = words.DocumentBuilder(doc)
-        builder.writeln(input.text)
+        builder.writeln(input)
         doc.save(f"{message.from_user.id}_output.docx")
         await msg.edit("Selesai ⬇️")
         await client.send_document(document=f"{message.from_user.id}_output.docx", chat_id=message.from_user.id)
