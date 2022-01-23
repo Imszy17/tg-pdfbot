8e8dac1cf3c619d25cbffff2095c7e7c732e985d
galihmrd <galihmardiana15@gmail.com>
2022-01-23 15:05:45 +0700

    improvement - added text2docx function - v1.0.2

--- lib/converter_document.py
+++ lib/converter_document.py
@@ -2,6 +2,7 @@ from pyrogram.types import Message
 from pyrogram import Client, filters
 
 from aspose import words
+from pyromod import listen
 
 
 @Client.on_message(filters.command("pdf2docx"))
@@ -19,4 +20,20 @@ async def pdf2docx_(client, message):
         else:
              await message.reply("Unsupported file!")
     except BaseException:
-        await message.reply("Balaskan perintah pada sebuah dokumen.pdf !")
+        await msg.edit("Balaskan perintah pada sebuah dokumen.pdf !")
+
+@Client.on_message(filters.command("text2docx"))
+async def text2docx_(client, message):
+    input = await client.ask(msg.chat.id,'Silahkan Tulis dan kirim teks yang akan dikonversi!', filters=filters.text, parse_mode='Markdown', disable_web_page_preview=True)
+    msg = await message.reply("Memproses...")
+    # Blank document
+    doc = words.Document()
+    # Write a document
+    try:
+        builder = words.DocumentBuilder(doc)
+        builder.writeln(input.text)
+        doc.save(f"{message.from_user.id}_output.docx")
+        await msg.edit("Selesai ⬇️")
+        await client.send_document(document=f"{message.from_user.id}_output.docx", chat_id=message.from_user.id)
+    except Exception as e:
+        await msg.edit(e)
