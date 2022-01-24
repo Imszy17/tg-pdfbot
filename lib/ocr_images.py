8b81d8ae1b5fbe3b58b0177ccde2f657593e83e1
galihmrd <galihmardiana15@gmail.com>
2022-01-24 17:02:23 +0700

    Send message dialog when command is not replied to the images, and improve

--- lib/ocr_images.py
+++ lib/ocr_images.py
@@ -30,19 +30,19 @@ async def ocr(client, message):
             return await message.reply(
                 "`Kode bahasa salah, atau tidak didukung!`", parse_mode="md"
             )
-    if replied.photo:
-        msg = await message.reply("`Memproses gambar...`")
-        image = await client.download_media(
-            replied, file_name=f"text_{message.from_user.id}.jpg"
-        )
-        img = Image.open(image)
-        text = pytesseract.image_to_string(img, lang=f"{lang_code}")
-        try:
-            await msg.delete()
-            await message.reply(text[:-1], quote=True, disable_web_page_preview=True)
-        except MessageEmpty:
-            return await message.reply("Teks tidak dapat diproses")
-    elif not replied.photo:
-        await message.reply("Balaskan perintah pada sebuah gambar!")
-    else:
-        await message.reply("input not found")
+    try:
+       if replied.photo:
+           msg = await message.reply("`Memproses gambar...`")
+           image = await client.download_media(
+               replied, file_name=f"text_{message.from_user.id}.jpg"
+           )
+           img = Image.open(image)
+           text = pytesseract.image_to_string(img, lang=f"{lang_code}")
+           try:
+               await msg.edit(text[:-1])
+           except MessageEmpty:
+               return await message.reply("Teks tidak dapat diproses")
+       else:
+           await message.reply("input not found")
+    except BaseException:
+       await message.reply("Balaskan perintah pada sebuah gambar!")
