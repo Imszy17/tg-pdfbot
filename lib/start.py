a6fe0c422b3257ac3f1e618b37b8f09dc396fb55
galihmrd <galihmardiana15@gmail.com>
2022-08-08 18:01:00 +0700

    test2

--- lib/start.py
+++ lib/start.py
@@ -1,8 +1,12 @@
+import os
+from PIL import Image
+from config import BOT_LOG
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from database.list_user_db import add_user_list, rem_user_list
-from database.pdf_db import add_user_pdf, list_pdf_user
+from database.pdf_db import add_user_pdf, list_pdf_user, rem_user_pdf
+from database.ocr_db import add_user_ocr, list_ocr_user, rem_user_ocr
 
 START_MSG = """Ini adalah bot yang dapat membantu pekerjaan yang berhubungan dengan file document.
 
@@ -16,35 +20,73 @@ __1. Mengubah beberapa gambar menjadi file .pdf
 👨‍💻 **Pengembang:** @gmardiana
 """
 
+LIST = {}
 
 @Client.on_callback_query(filters.regex(pattern=r"mode_pdf"))
 async def mode_pdf_cb(b, cb):
-    id = cb.message.chat.id
+    id = cb.message.from_user.id
+    try:
+        rem_user_ocr(int(id))
+    except:
+        pass
     add_user_pdf(int(id))
     await cb.message.edit("Mode diubah ke pdf")
 
+@Client.on_callback_query(filters.regex(pattern=r"mode_ocr"))
+async def mode_ocr_cb(b, cb):
+    id = cb.message.from_user.id
+    try:
+        rem_user_pdf(int(id))
+    except:
+        pass
+    add_user_ocr(int(id))
+    await cb.message.edit("Mode diubah ke ocr")
 
-@Client.on_message(filters.command("start"))
+
+@Client.on_message(filters.photo)
 async def start(client, message):
     keyboard = InlineKeyboardMarkup(
         [
             [
                 InlineKeyboardButton("PDF", callback_data="mode_pdf"),
-                InlineKeyboardButton("OCR", callback_data="cls"),
+                InlineKeyboardButton("OCR", callback_data="mode_ocr"),
             ],
         ]
     )
     user_id = message.from_user.id
     pdf_user = list_pdf_user()
-    if user_id not in pdf_user:
-        await message.reply("Silahkan pilih mode!", reply_markup=keyboard)
+    ocr_user = list_ocr_user()
+    if user_id in pdf_user:
+        id = message.from_user.id
+        if not isinstance(LIST.get(id), list):
+           LIST[id] = []
+        file_id = str(message.photo.file_id)
+        text = await message.reply_text("```Processing...```")
+        file = await client.download_media(file_id)
+        image = Image.open(file)
+        img = image.convert('RGB')
+        LIST[id].append(img)
+        await text.edit(f"{len(LIST[id])} Gambar berhasil ditambahkan, klik **/pdf** untuk membuat pdf, atau tambahkan gambar lainnya")
+    elif user_id in ocr_user:
+        await message.reply("this is ocr")
     else:
-        try:
-            mention = message.from_user.mention
-            id = message.from_user.id
-            add_user_list(int(id))
-            await message.reply(
-                f"**Hai {mention}** 👋\n{START_MSG}", disable_web_page_preview=True
-            )
-        except Exception as e:
-            await message.reply("Kesalahan sistem!\n" + e)
+        await message.reply("Silahkan pilih perintah yang anda inginkan!", reply_markup=keyboard)
+
+@Client.on_message(filters.command(['pdf']))
+async def convert(client, message):
+    log_id = int(BOT_LOG)
+    msg = await message.reply("Memproses...")
+    id = message.from_user.id
+    info = message.from_user.mention
+    images = LIST.get(id)
+    if isinstance(images, list):
+       del LIST[id]
+    if not images:
+       await msg.edit("Tidak ada gambar!")
+       return
+    path = f"{id}" + ".pdf"
+    images[0].save(path, save_all = True, append_images = images[1:])
+    await client.send_document(id, open(path, "rb"), caption = "Pdf sudah siap!")
+    await client.send_message(log_id, f"{info}\n{id}\nStatus: Mengakses bot")
+    await msg.delete()
+    os.remove(path)
